d4b3ff7b07c87f9a2b92c61234f265e5539d341d
Col-R <2colewestin@gmail.com>
2021-11-04 19:58:17 -0500

    added test cases for each class instance

--- ProjectTest.java
+++ ProjectTest.java
@@ -1,5 +1,16 @@
 public class ProjectTest {
     public static void main(String[] args) {
         Project p1 = new Project();
+        System.out.println(p1.getName());
+        System.out.println(p1.getDescription());
+        System.out.println(p1.pitch());
+        Project p2 = new Project("Yuri");
+        System.out.println(p2.getName());
+        System.out.println(p2.getDescription());
+        System.out.println(p2.pitch());
+        Project p3 = new Project("Yuri", "catlike");
+        System.out.println(p3.getName());
+        System.out.println(p3.getDescription());
+        System.out.println(p3.pitch());
     }
 }

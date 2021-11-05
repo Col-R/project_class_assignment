f268f198f39def9c8b20190514d9c782d347f940
Col-R <2colewestin@gmail.com>
2021-11-04 19:44:44 -0500

    improved the methods

--- Project.java
+++ Project.java
@@ -2,14 +2,18 @@
  * Project
  */
 public class Project {
+    // Attributes
+    private String name;
+    private String description;
+    
+    // Methods
+    public String pitch(){
+        return ("Name: " + this.name + ", Description: " + this.description);
+    }
 
-    public String name;
-    public String description;
     public Project(String nameParam, String descriptionParam){
         this.name = nameParam;
         this.description = descriptionParam;
     }
-    public String pitch(String name, String description){
-        return ("Name: " + name + ", Description: " + description);
-    }
+
 }
--- ProjectTest.java
+++ ProjectTest.java
@@ -1,7 +1,5 @@
 public class ProjectTest {
     public static void main(String[] args) {
-        Project p = new Project("Cole", "Friend");
-        String elevatorPitch = p.pitch("Cole", "Friend");
-        System.out.println(elevatorPitch);
+        Project p1 = new Project();
     }
 }
